5359bd2a29830668bf2e919ee32b22a70f485310
Zesty <adi8116315284@gmail.com>
2024-09-03 23:41:52 +0530

    implementing binary search algorithm

---
+++ dsa/array/binary_search.cpp
@@ -0,0 +1,40 @@
+
+//
+
+#include <iostream>
+
+using namespace std;
+
+int binarysearch(int arr[], int low, int high, int x);
+
+int main(void) {
+
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarysearch(arr, 0, n - 1, x);
+    if (result == -1) cout << "Element is not present in array";
+    else cout << "Element is present at index " << result;
+    return 0;
+}
+
+int binarysearch(int arr[], int low, int high, int x) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == x)
+            return mid;
+
+        // If x is greater, ignore left half
+        if (arr[mid] < x)
+            low = mid + 1;
+
+        // If x is lower, ignore right half
+        else
+            high = mid - 1;
+    }
+
+    // if we reach here, then the element was not present
+    return -1;
+}
